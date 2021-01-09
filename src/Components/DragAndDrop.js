67c56a3e756e7b8c7cf8bea4ce02955ca7c2f248
Yousef <yahmed1996a@gmail.com>
2021-01-09 13:53:36 +0000

    Added drop-zone package, tested to update state with both modal upload as well as dragging and dropping files into section

---
+++ src/Components/DragAndDrop.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+import Dropzone from 'react-dropzone'
+
+export default function DragAndDrop() {
+    const [fileList,setFileList] = useState([])
+
+
+    const addFileToList = (fileObject) =>{
+        let newObj = [...fileList]; // copying the old state
+        newObj.push(fileObject)
+        setFileList(newObj);
+
+    }
+
+
+    return (
+        <div>
+            <h1>Drag and drop</h1>
+            <Dropzone onDrop={acceptedFiles => addFileToList(acceptedFiles[0])}>
+                {({getRootProps, getInputProps}) => (
+                <section>
+                    <div {...getRootProps()}>
+                    <p>Drag files here to upload</p>
+                    <input {...getInputProps()} />
+                    <button>Upload File</button>
+                    </div>
+                </section>
+                )}
+            </Dropzone>
+
+            <button onClick={()=>{console.log(fileList)}}>Log fileList</button>
+        </div>
+    )
+}
--- src/Components/UploadContainer.js
+++ src/Components/UploadContainer.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import DragAndDrop from './DragAndDrop'
 
 export default function UploadContainer() {
     return (
         <div>
-            <h1>HelloWorld</h1>
+            <h1>Upload your Files</h1>
+            <DragAndDrop/>
+
+
         </div>
     )
 }
